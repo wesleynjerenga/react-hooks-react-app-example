b0c4c53afc46e8247894f8d93582f897b1501ee1
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-04-08 11:54:29 +0300

    1

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  presets: [
+  "presets": [
     "@babel/preset-env",
-    ["@babel/preset-react", { runtime: "automatic" }],
-  ],
+    ["@babel/preset-react", { "runtime": "automatic" }]
+  ]
 };
