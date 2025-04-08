4c1bf733037f0dc62d1606dd163577337eaae55d
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-04-08 12:00:21 +0300

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
+import TestComponent from "./TestComponent";
 
 // Add your code own within the return statement
 function App() {
   return (
     <div className="App">
-      <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
+      <header>
+        <h1>Now</h1> {/* Replace dynamic date/time with "Now" */}
+      </header>
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
@@ -14,6 +17,9 @@ function App() {
         self-contained chunks of JSX.
       </p>
       <ExampleComponent />
+      <div>
+        <TestComponent /> {/* Ensure TestComponent is rendered */}
+      </div>
     </div>
   );
 }
